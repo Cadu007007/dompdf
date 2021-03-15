37f7a6fd2034394c21f77f495c57d1b1a74e73f8
Amr <31748700+Cadu007007@users.noreply.github.com>
2021-03-15 12:26:25 +0200

    change text file

--- src/Renderer/Text.php
+++ src/Renderer/Text.php
@@ -11,6 +11,7 @@ namespace Dompdf\Renderer;
 
 use Dompdf\Adapter\CPDF;
 use Dompdf\Frame;
+use Arphp\Glyphs as Glyphs;
 
 /**
  * Renders text frames
@@ -81,6 +82,11 @@ class Text extends AbstractRenderer
           $text
         );*/
 
+        if ( !class_exists( 'I18N_Arabic' ) )
+	{  
+		$Arabic = new Glyphs('Glyphs');
+		$text = $Arabic->utf8Glyphs($text);
+	}
         $this->_canvas->text($x, $y, $text,
             $font, $size,
             $style->color, $word_spacing, $char_spacing);
